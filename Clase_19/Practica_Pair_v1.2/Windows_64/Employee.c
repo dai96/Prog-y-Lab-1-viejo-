4a8629fc32b8c74836845e4ee33cdcfc57a8aad8
Daira <36193806+dai96@users.noreply.github.com>
2018-06-07 12:19:28 -0300

    caja de herramientas parcial2

---
+++ Clase_19/Practica_Pair_v1.2/Windows_64/Employee.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+
+int employee_compare(void* pEmployeeA,void* pEmployeeB)
+{
+    Employee* e1;
+    Employee* e2;
+    int retorno;
+
+    e1=(Employee*)pEmployeeA;
+    e2=(Employee*)pEmployeeB;
+
+    retorno= strcmp(e1->name,e2->name);
+
+    return retorno;
+
+}
+
+void employee_print(Employee* this)
+{
+    printf("\n-----------\nID: %d\nNombre: %s\nApellido: %s",this->id,this->name,this->lastName);
+}
+
+
+int employee_new(Employee* lista)
+{
+
+
+    return 0 ;
+
+}
+
+void employee_delete(Employee* this)
+{
+
+
+}
+
+int employee_setId(Employee* this, int id)
+{
+
+    return 0;
+
+}
+
+int employee_getId(Employee* this)
+{
+
+    return 0;
+
+}
+
+
---
+++ Clase_19/Practica_Pair_v1.2/Windows_64/main.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+/****************************************************
+    Menu:
+        1. Parse del archivo data.csv
+        2. Listar Empleados
+        3. Ordenar por nombre
+        4. Agregar un elemento
+        5. Elimina un elemento
+        6. Listar Empleados (Desde/Hasta)
+*****************************************************/
+//primera parte del arcial, segunda par
+
+int main()
+{
+    int i;
+    int tamaniopunteo;
+    Employee* aux;
+    ArrayList* lista;
+    lista =al_newArrayList();//hace un malloc.
+    Employee* e1;
+    e1=(Employee*)malloc(sizeof(Employee));
+    if (e1!=NULL)
+    {
+        e1->id=4;
+        strcpy(e1->name,"Dai");
+        strcpy(e1->lastName,"Minarro");
+        al_add(lista,e1);
+        tamaniopunteo=al_len(lista);
+        //printf("%d",tamaniopunteo);-->en los dos casos me da 1?
+    }
+
+    Employee* e2;
+    e2=(Employee*)malloc(sizeof(Employee));
+    if (e2!=NULL)
+    {
+        e2->id=2;
+        strcpy(e2->name,"Pueds");
+        strcpy(e2->lastName,"Flo");
+        al_add(lista,e2);
+        //printf("\n%d",tamaniopunteo);En los dos casos me da 1?
+    }
+
+    for (i=0;i<2;i++)
+    {
+    aux=(Employee*)al_get(lista,i);
+    employee_print(aux);
+    }
+
+   al_sort(lista, employee_compare,0);
+
+
+   for (i=0;i<2;i++)
+    {
+    aux=(Employee*)al_get(lista,i);
+    employee_print(aux);
+    }
+
+    /*
+    //aux=al_get(lista,0); lo de abajo es mejor
+    aux=(Employee*)al_get(lista,0);
+    printf("\niD: %d",aux->id);
+    printf("\nNombre: %s",aux->name);
+    printf("\nApellido: %s",aux->lastName);
+    aux=(Employee*)al_get(lista,1);
+    printf("\niD: %d",aux->id);
+    printf("\nNombre: %s",aux->name);
+    printf("\nApellido: %s",aux->lastName);
+    */
+
+
+}
+
